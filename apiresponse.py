46f06133061d989ecdd24a3cf1f6aa8786f7adc3
jules <1161581685@qq.com>
2023-05-18 14:23:15 +0800

    api2

--- apiresponse.py
+++ apiresponse.py
@@ -2,7 +2,7 @@ import os
 import openai
 
  #api key 密钥需前往openai官网获取，每个人都不一样
-openai.api_key ="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" 
+openai.api_key ="" 
 
  #content后输入问题
 response = openai.ChatCompletion.create(
@@ -14,4 +14,15 @@ response = openai.ChatCompletion.create(
 
 #得到直接回答
 result = response.choices[0].message.content
-print(result)
+
+
+
+response2 = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "user", "content": result}
+  ]
+)
+
+result2 = response2.choices[0].message.content
+print(result2)
